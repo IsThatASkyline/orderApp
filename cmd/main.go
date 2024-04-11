aeecbb28a5d1244bd191d01b766bef98a02a9be5
artklk12 <artem-kulik@list.ru>
2024-04-11 12:12:37 +0300

    add fx

--- cmd/main.go
+++ cmd/main.go
@@ -1,28 +1,53 @@
 package main
 
 import (
-	"github.com/IsThatASkyline/fiberGo/internal/application/services"
+	"context"
 	postgresDB "github.com/IsThatASkyline/fiberGo/internal/infrastructure/db"
-	"github.com/IsThatASkyline/fiberGo/internal/infrastructure/factories"
-	orderRepo "github.com/IsThatASkyline/fiberGo/internal/infrastructure/factories/db"
-	"github.com/IsThatASkyline/fiberGo/internal/presentation/api/controllers/handlers"
-	"github.com/IsThatASkyline/fiberGo/internal/presentation/api/controllers/routes/order"
 	"github.com/gofiber/fiber/v3"
 	_ "github.com/lib/pq"
-	"log"
+	"go.uber.org/fx"
 )
 
+/*
 func main() {
 	// Initialize a new Fiber app
+	app := fiber.New()
+
 	orderRepository := orderRepo.BuildOrderRepo(factories.NewBaseRepo(postgresDB.BuildConnection()))
 	orderService := services.NewOrderService(orderRepository)
 
 	// Define a route for the GET method on the root path '/'
 	handler := handlers.NewHandler(orderService)
 	newRoutes := order.NewOrderRoutes(handler)
-	app := fiber.New()
 	newRoutes.Setup(app)
 
 	// Start the server on port 8000
 	log.Fatal(app.Listen(":8080"))
+}
+*/
+
+func registerHooks(
+	lifecycle fx.Lifecycle,
+	app *fiber.App,
+) {
+	lifecycle.Append(
+		fx.Hook{
+			OnStart: func(context.Context) error {
+				go app.Listen(":8080")
+				return nil
+			},
+			OnStop: func(context.Context) error {
+				return nil
+			},
+		},
+	)
+}
+
+func main() {
+	fx.New(
+		fx.Provide(postgresDB.BuildConnection),
+		fx.Provide(fiber.New),
+		fx.Invoke(registerHooks),
+	).Run()
+	// Initialize a new Fiber app
 }
